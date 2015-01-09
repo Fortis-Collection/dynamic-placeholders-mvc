cb02f688f14c757bb19e4f358a62b8a2a61013e1
Jason Bert <jason.bert@goblinrockets.com>
2015-01-09 13:17:01 +0000

    Added code for using dynamic placeholders with Sitecore MVC

---
+++ Source/DynamicPlaceholders.Mvc/Extensions/SitecoreHelperExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Sitecore.Mvc.Helpers;
+using Sitecore.Mvc.Presentation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace DynamicPlaceholders.Mvc.Extensions
+{
+	public static class SitecoreHelperExtensions
+	{
+		public static HtmlString DynamicPlaceholder(this SitecoreHelper helper, string placeholderName)
+		{
+			var currentRenderingId = RenderingContext.Current.Rendering.UniqueId;
+
+			return helper.Placeholder(string.Format("{0}_{1}", placeholderName, currentRenderingId));
+		}
+	}
+}
